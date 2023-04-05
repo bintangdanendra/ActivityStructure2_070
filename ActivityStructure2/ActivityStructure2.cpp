218f676f3e2d1b58346e22ed315fec2a9d83f3e8
BintangDanendra <bintang.danendra140704@gmail.com>
2023-04-05 11:48:55 +0700

    first commite

--- ActivityStructure2/ActivityStructure2.cpp
+++ ActivityStructure2/ActivityStructure2.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+using namespace std;
 
-int main()
+struct DetailAlamat
 {
-    std::cout << "Hello World!\n";
-} 
+    char desa[20];
+    char kota[20];
+};
+
+struct mahasiswa
+{
+    char nim[12];
+    char nama[20];
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    mahasiswa mhs;
+
+
+    cout << "Masukkan NIM : ";
+    cin.getline(mhs.nim, 12);
+    cout << "Masukkan Umur : ";
+    cin >> mhs.umur;
+    cin.ignore(1, '\n');
+    cout << "Masukkan Nama : ";
+    cin.getline(mhs.nama, 20);
+    cout << "Masukkan Alamat : ";
+    cout << "\n\tMasukkan Desa : ";
+    cin.getline(mhs.alamat.desa, 20);
+    cout << "\tMasukkan Kota : ";
+    cin.getline(mhs.alamat.kota, 20);
+
+
+    cout << "\n\nNIM : " << mhs.nim;
+    cout << "\nUmur : " << mhs.umur;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nAlamat : ";
+    cout << "\n\tDesa : " << mhs.alamat.desa;
+    cout << "\n\tKota : " << mhs.alamat.kota;
+
+}
